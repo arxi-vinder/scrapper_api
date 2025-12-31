fc8d867e173e0e14fe0f022cc9aa4ebc216b0add
KrishnanditoLksn <krislaksana123@gmail.com>
2025-12-31 11:45:28 +0700

    feat:set all router in servr file

--- app/server.py
+++ app/server.py
@@ -1,4 +1,15 @@
 from fastapi import FastAPI
 
+from app.api.v1 import scrap
+
 
 app = FastAPI()
+
+@app.get("/")
+def check_health():
+    return {
+        "status":"Success",
+        "message":"Api Is Online"
+    }
+
+app.include_router(scrap.router)
