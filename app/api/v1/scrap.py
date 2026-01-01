1c4d320f9bd915c268266a389f5b4baa6bbd44cd
KrishnanditoLksn <krislaksana123@gmail.com>
2026-01-01 09:35:42 +0700

    feat:add save to csv from scrapping results

--- app/api/v1/scrap.py
+++ app/api/v1/scrap.py
@@ -2,10 +2,11 @@ import asyncio
 import random
 from bs4 import BeautifulSoup
 from fastapi import APIRouter
+from fastapi.concurrency import run_in_threadpool
 import httpx
 
 from app.schemas.request.category_arxiv import CategoryArxivRequest
-from app.utils.arxiv_helper import arxiv_pages, extract_id, fetchArxivId, scrape_all_details
+from app.utils.arxiv_helper import arxiv_pages, extract_id, fetchArxivId, save_to_csv, scrape_all_details
 
 
 router = APIRouter(
@@ -46,3 +47,17 @@ async def fetch_arxiv_content(data: CategoryArxivRequest):
         "data": content
     }
 
+
+@router.post("/save-csv")
+async def save_csv(data: CategoryArxivRequest):
+    pages = arxiv_pages(data.arxiv_fields)
+
+    arxiv_ids = await fetchArxivId(pages)
+
+    content = await scrape_all_details(arxiv_ids)
+
+    await run_in_threadpool(save_to_csv, content)
+    return {
+        "status": "success",
+        "saved_rows": len(content)
+    }
